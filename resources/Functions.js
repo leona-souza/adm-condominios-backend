70ea9b47555ab480302e910bcbde2cb76e50201e
Richard Souza <richard321@gmail.com>
2021-04-12 16:57:54 -0300

    total de registros na paginação

--- resources/Functions.js
+++ resources/Functions.js
@@ -4,6 +4,7 @@ paginatedResults = (modelo, sort) => {
         const limite = parseInt(req.query.limite);
         const inicioIndex = (pagina - 1) * limite;
         const finalIndex = pagina * limite;
+        const total = await modelo.countDocuments().exec();
         const listaResultados = {};
 
         if (inicioIndex > 0) {
@@ -12,13 +13,14 @@ paginatedResults = (modelo, sort) => {
                 limite: limite
             };
         }
-        if (finalIndex < await modelo.countDocuments().exec()) {
+        if (finalIndex < total) {
             listaResultados.proximo = {
                 paginaProxima: pagina + 1,
                 limite: limite
             };
         }
 
+        listaResultados.total = total;
         listaResultados.resultados = await modelo.find().sort(sort).limit(limite).skip(inicioIndex).exec();
         res.paginatedResults = listaResultados;
         next();
